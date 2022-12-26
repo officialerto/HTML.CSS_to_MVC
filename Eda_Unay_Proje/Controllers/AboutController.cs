124802ae9618e9e5ce62261aa496f0bd8e019585
Ertugrul <57365047+officialerto@users.noreply.github.com>
2022-12-26 15:01:58 +0300

    Add project files.

---
+++ Eda_Unay_Proje/Controllers/AboutController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eda_Unay_Proje.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
